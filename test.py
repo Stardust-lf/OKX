d1c099d7232e6812501f1e16862da909fb809b3c
Stardust-lf <lifan20010110@163.com>
2024-02-09 22:55:27 +0800

    Base query operations achieved

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from query import WalletQuery, CoinQuery, PriceQuery
+from base_cls.base_operator import BaseOperator
 
-wq = WalletQuery()
-print(wq.fetch_response())
-cq = CoinQuery(ccy='BTC,ETH')
-print(cq.fetch_response())
-pq = PriceQuery('BTC','USDT')
-print(pq.fetch_response())
+
+# wq = WalletQuery()
+# print(wq.fetch_response())
+# cq = CoinQuery(ccy='BTC,ETH')
+# print(cq.fetch_response())
+# pq = PriceQuery('BTC','USDT')
+# print(pq.fetch_response())
+def test_operator(operator: BaseOperator):
+    print(operator.fetch_response())
+
+
+if __name__ == '__main__':
+    test_operator(WalletQuery())
+    test_operator(CoinQuery('BTC,ETH'))
+    test_operator(PriceQuery('BTC', 'USDT'))
